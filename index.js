d18726b908e0c4e0605a945fbb510a06e0db62c4
Anita Beauchamp <amadamspr@gmail.com>
2018-01-14 01:47:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-const app = "I don't do much."
+//var kittens = ["Milo","Otis","Garfield"];
+
+function destructivelyAppendKitten(kittenName) {
+  kittens.push(kittenName);
+  
+}
+
+function destructivelyPrependKitten(kittenName) {
+  kittens.unshift(kittenName);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(kittenName) {
+  return [...kittens,kittenName];
+}
+
+function prependKitten(kittenName) {
+  return [kittenName,...kittens];
+}
+
+function removeLastKitten() {
+  return kittens.slice(0,kittens.length - 1);
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1,kittens.length);
+}
