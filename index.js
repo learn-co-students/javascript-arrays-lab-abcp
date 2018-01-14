46f11bc8f4fff63ee82ce90c3fdba4b350d108f3
Dylan Ferrara <dylan.ferrara@gmail.com>
2018-01-14 07:54:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift();
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop();
+}
+
+function appendKitten(name) {
+  var kit = kittens.slice();
+  kit.push(name);
+  return kit;
+}
+
+function prependKitten(name) {
+  var kit = kittens.slice();
+  kit.unshift(name);
+  return kit;
+}
+
+function removeLastKitten() {
+  var kit = kittens.slice();
+  kit.pop();
+  return kit;
+}
+
+function removeFirstKitten() {
+  var kit = kittens.slice();
+  kit.shift();
+  return kit;
+}
