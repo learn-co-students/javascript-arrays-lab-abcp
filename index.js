3581febd33fdbeee46995f5c8f373b6829f9cb10
geoffrey woodcock <theatomproject@gmail.com>
2018-01-14 20:00:23 +0000

    Done.

--- index.js
+++ index.js
@@ -4,3 +4,34 @@ function destructivelyAppendKitten(name){
   kittens.push(name)
   return kittens
 }
+kittens = ['Milo', 'Otis', 'Garfield']
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
+kittens = ['Milo', 'Otis', 'Garfield']
+function destructivelyRemoveLastKitten(name){
+  kittens.pop(name)
+  return kittens
+}
+kittens = ['Milo', 'Otis', 'Garfield']
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift(name)
+  return kittens
+}
+kittens = ['Milo', 'Otis', 'Garfield']
+function appendKitten(name){
+  return [...kittens, name]
+}
+kittens = ['Milo', 'Otis', 'Garfield']
+function prependKitten(name){
+  return [name,...kittens]
+}
+kittens = ['Milo', 'Otis', 'Garfield']
+function removeLastKitten(name){
+  return kittens.slice(0, kittens.length - 1)
+}
+kittens = ['Milo', 'Otis', 'Garfield']
+function removeFirstKitten(name){
+  return kittens.slice(1)
+}
