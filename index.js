7d37787c494dcd763be858ccbfacb4c5db1b4473
A Lee <ayslee@gmail.com>
2018-01-15 19:58:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+var kittens = ['Milo', 'Otis', 'Garfield'];
+console.log(kittens);
+
+destructivelyAppendKitten(array, element); {
+  array.push(element);
+  return array;
+}
+
+console.log(destructivelyAppendKitten(kittens, "Ralph"));
