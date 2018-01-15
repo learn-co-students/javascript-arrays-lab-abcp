2a25a61ab787b835127703d34cc71f6b74765b7f
Laura Nadolski <laura.nadolski@gmail.com>
2018-01-15 01:48:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+//appends a kitten to the end of the kittens array
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+
+//prepends a kitten to the beginning of the kittens array
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+}
+
+//removes the last kitten from the kittens array
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+
+//removes the First kitten from the kittens array
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+
+//appends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged
+function appendKitten(name){
+  var newkittenarray = [...kittens, name];
+  return newkittenarray;
+}
+
+//prepends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged
+function prependKitten(name){
+  var newkittenarray = [name,...kittens];
+  return newkittenarray;
+}
+
+//removes the last kitten in the kittens array and returns a new array, leaving the kittens array unchanged
+function removeLastKitten(){
+  var newkittenarray = kittens.slice(0, kittens.length -1);
+  return newkittenarray;
+}
+
+//removes the first kitten from the kittens array and returns a new array, leaving the kittens array unchanged
+function removeFirstKitten(){
+  var newkittenarray = kittens.slice(1);
+  return newkittenarray;
+}
