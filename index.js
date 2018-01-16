1abb48bbd426fa9765bb6e1042b24b1e81b46d17
River Magnetic <magneticcontinuum@gmail.com>
2018-01-16 13:28:07 +0000

    Done.

--- index.js
+++ index.js
@@ -20,3 +20,18 @@ function appendKitten(name){
   var newKittens = [...kittens, name]
   return newKittens
 }
+
+function prependKitten(name){
+  var newKittens = [name, ...kittens]
+  return newKittens
+}
+
+function removeLastKitten(){
+  var newKittens = kittens.slice(0, kittens.length -1)
+  return newKittens
+}
+
+function removeFirstKitten() {
+  var newKittens = kittens.slice(1)
+  return newKittens
+}
