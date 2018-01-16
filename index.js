f7a0562159f34942a3afe1ff384351eca9ddb648
Hany Rostom <hanyrostom@gmail.com>
2018-01-16 21:46:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  return kittens.push(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  return kittens.pop();
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+
+function appendKitten(name) {
+  let newKittens = [...kittens,(name)];
+  return newKittens;
+}
+
+function prependKitten(name) {
+  let newKittens = [(name),...kittens];
+  return newKittens;
+}
+
+function removeLastKitten() {
+
+  return kittens.slice(0,kittens.length-1);
+}
+
+function removeFirstKitten() {
+
+  return kittens.slice(1,kittens.length);
+}
