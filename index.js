cf51a02b57355f839c34f5a45029ae07f4c3acb4
stanciuionelamelania@gmail.com <stanciuionelamelania@gmail.com>
2018-01-19 22:48:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.reverse()
+  kittens.pop()
+  kittens.reverse()
+  return kittens
+}
+function destructivelyPrependKitten(name) {
+ kittens.reverse()
+ kittens.push(name)
+ kittens.reverse()
+ return kittens
+}
+function appendKitten(name){
+  var kittens_copy = kittens.slice()
+  kittens_copy.push(name)
+  return kittens_copy
+}
+
+function prependKitten(name){
+var kittens_copy = kittens.slice()
+kittens_copy.unshift(name)
+return kittens_copy
+}
+
+function removeLastKitten() {
+  var kittens_copy = kittens.slice()
+  kittens_copy.pop()
+  return kittens_copy
+}
+function removeFirstKitten() {
+    var kittens_copy = kittens.slice()
+    kittens_copy.shift()
+    return kittens_copy
+}
