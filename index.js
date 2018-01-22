435309fe37af87e3af421d74f79ceb0ef616921b
leahbateman@gmail.com Bateman <leahbateman@gmail.com>
2018-01-22 01:25:03 +0000

    Done.

--- index.js
+++ index.js
@@ -10,3 +10,12 @@ function destructivelyPrependKitten(name) {
   return kittens
 }
 
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
