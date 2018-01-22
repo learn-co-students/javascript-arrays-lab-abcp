882f65db02bdbcfbd0ba13454ddd7547398ff552
leahbateman@gmail.com Bateman <leahbateman@gmail.com>
2018-01-22 01:23:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 var kittens = ["Milo", "Otis", "Garfield"]
 
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
