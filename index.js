8cf54bb9ad9e567247d5a6473f624bd4d5c596ac
Charlotte Payne <cltpayne@gmail.com>
2018-01-23 23:12:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  var newkittens = kittens.push('Ralph');
+}
+
+function destructivelyPrependKitten(name) {
+  var newkittens = kittens.unshift('Bob');
+}
+
+function destructivelyRemoveLastKitten(name) {
+  var newkittens = kittens.pop('Bob');
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  var newkittens = kittens.shift('Bob');
+}
+
+function appendKitten(name) {
+   var kittens2 = [...kittens, 'Broom'];
+   return kittens2
+}
+
+function prependKitten(name) {
+  var kittens3 = ['Arnold', ...kittens]
+  return kittens3
+}
+
+function removeLastKitten() {
+  var kittens4 = kittens.slice(0,2)
+  return kittens4
+}
+
+function removeFirstKitten() {
+  var kittens5 = kittens.slice(1)
+  return kittens5
+}
+
